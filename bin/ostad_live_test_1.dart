ff764dc21467fafa1b7d98d57c13bb05dd0a3c58
jubairmoaj <jubairmoaj71@gmail.com>
2024-03-16 00:48:27 +0600

    Initial commit

---
+++ bin/ostad_live_test_1.dart
@@ -0,0 +1,5 @@
+import 'package:ostad_live_test_1/ostad_live_test_1.dart' as ostad_live_test_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ostad_live_test_1.calculate()}!');
+}
---
+++ test/ostad_live_test_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_live_test_1/ostad_live_test_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
